60016f3231157f9bcdfe6f8d0dcc7a5727422563
Lauren Talbot <ltalbot@barkbox.com>
2023-05-02 10:04:12 -0400

    documenting

--- predict_open_invoices/evaluation_on_csv_data.py
+++ predict_open_invoices/evaluation_on_csv_data.py
@@ -40,14 +40,14 @@ def create_neptune_csv_model() -> neptune.Model:
 
 
 def get_data_splits(keys: list[str] = ['train', 'test']) -> list[pandas.DataFrame]:
+    """Return specified split data frames from neptune model."""
     if len(keys) == 0:
         return keys
     frames = []
     for key in keys:
         temp_path = tempfile.NamedTemporaryFile().file.name
         NEPTUNE_MODEL[f'data/{key}'].download(temp_path)
-        df = pandas.read_pickle(temp_path)
-        frames.append(df)
+        frames.append(pandas.read_pickle(temp_path))
     return frames
 
 
@@ -56,8 +56,9 @@ TRAIN_H2O_FRAME, TEST_H2O_FRAME = get_h2o_frame(TRAIN_DF), get_h2o_frame(TEST_DF
 
 
 def get_monthly_forecast_error(df: pandas.DataFrame, h2o_model: h2o.estimators.H2OEstimator,
-                               y: str = 'collected_per_month'):
-    """Given a slice of data, outcome variable """
+                               y: str = 'collected_per_month') -> float:
+    """Given a slice of data, outcome variable, and trained model, calculate the mean absolute percentage diff between
+     monthly amount collected (normalized by company) and monthly amount forecasted for collection.."""
     predictions = predict(df, h2o_model)
     results = df[['collected_per_month', 'month_collected', 'inv_pct_of_company_total']].copy()
     if y == 'collected_per_month':
@@ -75,6 +76,7 @@ def get_monthly_forecast_error(df: pandas.DataFrame, h2o_model: h2o.estimators.H
 
 def train_model_version(params: dict = dict(metric='mae', predictors=['due_per_month'], y='collected_per_month',
                                             distribution='huber', max_runtime_secs=60)) -> float:
+    """Given a set of hyperparameters, train a model version and record performance statistics in neptune."""
     neptune_model_version = neptune.init_model_version(model=f"{NEPTUNE_PROJECT_ID}-{NEPTUNE_MODEL_ID}",
                                                        project=NEPTUNE_PROJECT_NAME)
     neptune_model_version['predictors'] = stringify_unsupported(params['predictors'])
@@ -85,7 +87,6 @@ def train_model_version(params: dict = dict(metric='mae', predictors=['due_per_m
     neptune_model_version['ml_log_metrics'] = stringify_unsupported(['r2', 'mae', 'rmsle'])
     neptune_model_version['nfolds'] = params['nfolds']
     #neptune_model_version['stopping_tolerance'] = params['stopping_tolerance']
-
     h2o_model = train_model(TRAIN_H2O_FRAME, TEST_H2O_FRAME, **params)
     neptune_model_version['monthly_mape_train'] = get_monthly_forecast_error(TRAIN_DF, h2o_model, y=params['y'])
     mape_test = get_monthly_forecast_error(TEST_DF, h2o_model, y=params['y'])
@@ -107,11 +108,13 @@ def train_model_version(params: dict = dict(metric='mae', predictors=['due_per_m
 
 
 def optuna_objective(trial):
+    """Use optuna to perform Bayesian optimization over a range of hyperparameters,
+    saving all trained model versions in neptune."""
     #y = trial.suggest_categorical('y', ['collected_per_month', 'month_collected'])
     # predictors = ['due_per_month', 'root_exchange_rate_value_inv', 'amount_inv',
     #               'remaining_inv_pct', 'months_open']
     y = 'collected_per_month'
-    predictors = ['months_allowed', 'months_open', 'remaining_inv_pct', 'amount_inv']
+    predictors = ['months_allowed', 'months_open', 'remaining_inv_pct', 'inv_pct_of_company_total']
     predictors += ['due_per_month'] if y == 'collected_per_month' else ['month_due']
     params = \
         {'y': y, 'predictors': predictors, 'metric': 'mae',
